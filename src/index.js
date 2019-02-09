6f90325031e2123d44074ed5ffe344cc21cf46a4
Diana Curtis <dianacurtis92@gmail.com>
2019-02-09 11:12:33 -0800

    finished the react-intro feature set

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const user={
+    name: 'Stuart',
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+
+function luckyNumber(){
+    return (Math.floor((Math.random()*1000))+1);
+}
+
+function greeting(props){
+    const {name,luckyNumber}=props;
+    return(
+        <div className="container">
+            <h1>Welcome {name}</h1>
+            <h2 className="text-muted">Your lucky number is: {luckyNumber}</h2>
+        </div>
+    )
+}
