21d3377fbf20f44094ad00599d88cef6a095d687
Diana Curtis <dianacurtis92@gmail.com>
2019-02-11 20:42:28 -0800

    finished upto and including featureset four

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        firstName:'Clark',
+        lastName:'Kent',
+        phone:'555-123-4567',
+        email: 'superman@google.com'
+    },
+    {
+        firstName:'Diana',
+        lastName:'Princess',
+        phone:'555-891-2345',
+        email: 'dianap@google.com'
+    },
+    {
+        firstName:'Peter',
+        lastName:'Parker',
+        phone:'555-167-',
+        email: 'spidy@google.com'
+    }
+];
